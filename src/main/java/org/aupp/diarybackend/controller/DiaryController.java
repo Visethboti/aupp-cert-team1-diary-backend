c8ec067002ff620913b1dd9466d43d9ff90386b7
Boti <s.visethboti.gmail.com>
2023-09-15 14:56:39 +0700

    update to/v1

--- src/main/java/org/aupp/diarybackend/controller/DiaryController.java
+++ src/main/java/org/aupp/diarybackend/controller/DiaryController.java
@@ -11,7 +11,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/diaries")
+@RequestMapping("/v1/diaries")
 public class DiaryController {
     @Autowired
     DiaryService diaryService;
