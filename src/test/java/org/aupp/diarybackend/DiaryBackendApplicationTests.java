cc8634ba53b329f57d09b72da1850e6acb43368d
Boti <s.visethboti.gmail.com>
2023-09-15 10:17:19 +0700

    Initized spring boot

---
+++ src/test/java/org/aupp/diarybackend/DiaryBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.aupp.diarybackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiaryBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
